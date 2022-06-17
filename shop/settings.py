68d6d1b8b20a20b718bb52c56d973b1fe8e743ee
skantrop <akimbaeva.a23@gmail.com>
2022-06-17 17:59:28 +0600

    last h

--- shop/settings.py
+++ shop/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': config('DB_ENGINE'),
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', cast=int)
+        # 'NAME': config('DB_NAME'),
+        # 'USER': config('DB_USER'),
+        # 'PASSWORD': config('DB_PASSWORD'),
+        # 'HOST': config('DB_HOST'),
+        # 'PORT': config('DB_PORT', cast=int)
     }
 }
 import dj_database_url
